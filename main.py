28b4b2c64f403250c88ab67d1f0be9891bb8ecba
Treigh-Tech <232130879+Treigh-Tech@users.noreply.github.com>
2026-01-01 17:19:05 -0700

    Update the project files

---
+++ main.py
@@ -0,0 +1,8 @@
+python:  print('hello class')
+
+def say_hello(): 
+    return 'hello'
+
+result = say_hello()
+
+print(result)
---
+++ run.sh
@@ -0,0 +1,11 @@
+reun.sh: #!/usr/bin/env bash
+
+echo "Running the script..."
+
+echo "Current directory:"
+pwd
+
+echo "Listing files:"
+ls -al
+
+echo "Done."
